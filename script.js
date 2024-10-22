d9fd3ff2a4e0cd6662dc61038bd6d4165282c86a
L11nas <ulevicius.linas@gmail.com>
2024-10-22 17:17:02 +0300

    stiliu atnaujinimas

--- script.js
+++ script.js
@@ -1,107 +1,102 @@
-gsap.to('.title-1', {
-  duration: 1,
-  x: 30,
-  opacity: 1,
-  ease: 'expo.inOut',
-  repeat: 0,
-  yoyo: false,
-});
+document.addEventListener('DOMContentLoaded', function () {
+  // Funkcija saugiam elementų gavimui ir animavimui
+  function animateElement(selector, animation) {
+    const elements = gsap.utils.toArray(selector);
+    if (elements.length) {
+      gsap.to(elements, animation);
+    }
+  }
 
-gsap.to('.title-2', {
-  delay: 1,
-  duration: 1,
-  opacity: 1,
-  ease: 'expo.inOut',
-  repeat: 0,
-  yoyo: false,
-});
-gsap.from('.runner', {
-  delay: 1.6,
-  duration: 2,
-  opacity: 0,
-  ease: 'expo.inOut',
-});
-gsap.from('.box-1', {
-  delay: 1,
-  duration: 4,
-  rotation: 200,
-  transformOrigin: '50% 50%',
-  opacity: 0,
-  x: -180,
-  ease: 'expo.inOut',
-});
-gsap.from('.box-2', {
-  delay: 1.2,
-  duration: 4,
-  rotation: 90,
-  transformOrigin: '50% 50%',
-  opacity: 0,
-  x: -180,
-  ease: 'expo.inOut',
-});
-gsap.from('.box-3', {
-  delay: 1.2,
-  duration: 4,
-  rotation: 180,
-  transformOrigin: '50% 50%',
-  opacity: 0,
-  x: -180,
-  ease: 'expo.inOut',
-});
-gsap.from('.pattern', {
-  delay: 0.8,
-  duration: 2,
-  width: 0,
-  opacity: 0,
-  ease: 'expo.inOut',
-});
-gsap.from('.logo', {
-  delay: 1.6,
-  duration: 2,
-  y: 20,
-  opacity: 0,
-  ease: 'expo.inOut',
-});
-gsap.from('.menu ul li', {
-  delay: 1.6,
-  duration: 2,
-  y: 20,
-  opacity: 0,
-  ease: 'expo.inOut',
-  stagger: 0.1,
-});
-gsap.from('.cart', {
-  delay: 1.7,
-  duration: 2,
-  y: 20,
-  opacity: 0,
-  ease: 'expo.inOut',
-});
-gsap.from('.media ul li', {
-  delay: 2,
-  duration: 2,
-  y: 20,
-  opacity: 0,
-  ease: 'expo.inOut',
-  stagger: 0.1,
-});
-gsap.from('.content p', {
-  delay: 2.4,
-  duration: 2,
-  y: 20,
-  opacity: 0,
-  ease: 'expo.inOut',
-});
+  // Funkcija saugiam elementų gavimui ir animavimui iš pradinės pozicijos
+  function animateElementFrom(selector, animation) {
+    const elements = gsap.utils.toArray(selector);
+    if (elements.length) {
+      gsap.from(elements, animation);
+    }
+  }
 
-gsap.from('.content button', {
-  delay: 2,
-  duration: 1,
-  y: 20,
-  opacity: 0,
-  ease: 'expo.inOut',
-  clearProps: 'all', // Išvalo GSAP pridėtas savybes po animacijos
-});
+  gsap.to('.title-1', {
+    duration: 1,
+    x: 30,
+    opacity: 1,
+    ease: 'expo.inOut',
+    repeat: 0,
+    yoyo: false,
+  });
 
+  gsap.to('.title-2', {
+    delay: 1,
+    duration: 1,
+    opacity: 1,
+    ease: 'expo.inOut',
+    repeat: 0,
+    yoyo: false,
+  });
+  gsap.from('.runner', {
+    delay: 1.6,
+    duration: 2,
+    opacity: 0,
+    ease: 'expo.inOut',
+  });
+  gsap.from('.box-1', {
+    delay: 1,
+    duration: 4,
+    rotation: 200,
+    transformOrigin: '50% 50%',
+    opacity: 0,
+    x: -180,
+    ease: 'expo.inOut',
+  });
+  gsap.from('.box-2', {
+    delay: 1.2,
+    duration: 4,
+    rotation: 90,
+    transformOrigin: '50% 50%',
+    opacity: 0,
+    x: -180,
+    ease: 'expo.inOut',
+  });
+  gsap.from('.box-3', {
+    delay: 1.2,
+    duration: 4,
+    rotation: 180,
+    transformOrigin: '50% 50%',
+    opacity: 0,
+    x: -180,
+    ease: 'expo.inOut',
+  });
+  gsap.from('.pattern', {
+    delay: 0.8,
+    duration: 2,
+    width: 0,
+    opacity: 0,
+    ease: 'expo.inOut',
+  });
+
+  gsap.from('.media ul li', {
+    delay: 2,
+    duration: 2,
+    y: 20,
+    opacity: 0,
+    ease: 'expo.inOut',
+    stagger: 0.1,
+  });
+  gsap.from('.content p', {
+    delay: 2.4,
+    duration: 2,
+    y: 20,
+    opacity: 0,
+    ease: 'expo.inOut',
+  });
+
+  gsap.from('.content button', {
+    delay: 2,
+    duration: 1,
+    y: 20,
+    opacity: 1,
+    ease: 'expo.inOut',
+  });
+});
 function toggleMenu() {
   const navMenu = document.querySelector('.nav-menu');
   if (navMenu) {
